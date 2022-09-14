8d6b162718a1fc95e9c4b88d711ac40916ce8ae0
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2022-09-14 13:53:19 +0530

    fourth commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className='bg-[#F0F2F2] tracking-wider'>
       {/* Header */}
       <div className="sticky top-0 left-0 font-mono font-bold bg-[#393E59] text-[#F0F2F2] tracking-normal">
-        <ul className="flex max-w-[65%] mx-auto justify-evenly py-4 text-lg">
+        <ul className="flex flex-col max-w-[85%] mx-auto justify-evenly text-center py-4 text-lg md:flex-row md:max-w-[80%]">
           <li className="px-2 py-1">
             <Link href="/">
               <span className="relative after:absolute after:w-[100%] after:scale-x-0 after:h-[2px] after:bottom-0 after:left-0 after:bg-[#F0F2F2] after:origin-bottom-right after:transition-transform after:delay-[0.1s] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left cursor-pointer">Home</span>
@@ -54,8 +54,8 @@ export default function Home() {
       <div className="font-mono bg-[#393E59] text-[#F0F2F2] text-center" id="AboutMe">
         <div className="pt-6 pb-10">
           <p className="text-[60px] font-bold mb-6">About Me</p>
-          <div className="flex justify-center max-w-[70%] mx-auto">
-            <div className="flex flex-col text-right">
+          <div className="flex flex-col-reverse justify-center max-w-[70%] mx-auto md:flex-row">
+            <div className="flex flex-col text-left md:text-right">
               <p className="text-[32px]">My name is Harshil Prajapati.</p>
               <p className="text-[22px] mt-4">I have 5 months of experience in front end web development and a 3 month experience as a graphic designer. I am passionate about designing and making new products using my imagination and creativity into it. I am flexible with any new technology as I am quick learner too. I also have completed online course of Adobe Illustrator and Photohsop to boost creativity in my designing.</p>
               <div className="flex flex-col text-[20px] mt-8">
@@ -65,7 +65,7 @@ export default function Home() {
                 <p>Languages Known: English, Hindi, Gujarati</p>
               </div>
             </div>
-            <img src="MyPic.jpg" className="w-[350px] h-[350px] rounded-[30px] border-[5px] border-[#F0F2F2] ml-12" alt="" />
+            <img src="MyPic.jpg" className="w-[350px] h-[350px] rounded-[30px] border-[5px] border-[#F0F2F2] ml-12 mb-4 md:mb-0" alt="" />
           </div>
         </div>
       </div>
@@ -74,9 +74,9 @@ export default function Home() {
       <div className="font-mono text-[#010326] text-center" id="Education">
         <div className="pt-6 pb-10">
           <p className="text-[60px] font-bold mb-6">Education</p>
-          <div className="flex justify-between max-w-[70%] mx-auto">
+          <div className="flex flex-col justify-between max-w-[70%] mx-auto md:flex-row">
             {/* 1 */}
-            <div className='flex flex-col max-w-[500px]'>
+            <div className='flex flex-col max-w-[500px] mb-8 md:mb-0'>
               <p className='font-bold text-[30px]'>Bachelors of Technology</p>
               <p className='text-[20px]'>
                 Indian Institute of Information Technology, Surat
@@ -86,7 +86,7 @@ export default function Home() {
               </p>
             </div>
             {/* 2 */}
-            <div className="flex flex-col max-w-[500px]">
+            <div className="flex flex-col max-w-[500px] mb-8 md:mb-0">
               <p className="font-bold text-[30px]">Higher Secondary(12th)</p>
               <p className="text-[20px]">
                 Smt. V.D. Desai (Wadiwala) Higher Secondary School, Surat
@@ -111,8 +111,9 @@ export default function Home() {
       <div className="font-mono bg-[#393E59] text-[#F0F2F2] text-center" id="WorkExperience">
         <div className="pt-6 pb-10">
           <p className="text-[60px] font-bold mb-6">Work Experience</p>
-          <div className="flex justify-between max-w-[70%] mx-auto">
-            <div className="flex flex-col mx-auto max-w-[500px]">
+          <div className="flex flex-col justify-between max-w-[70%] mx-auto md:flex-row">
+            {/* 1 */}
+            <div className="flex flex-col mx-auto max-w-[500px] mb-8 md:mb-0">
               <p className="font-bold text-[30px]">The Dairing Company</p>
               <p className="text-[20px]">
                 Front End Web Developer
@@ -120,6 +121,7 @@ export default function Home() {
                 <br/>January 2022 - June 2022
               </p>
             </div>
+            {/* 2 */}
             <div className="flex flex-col mx-auto max-w-[500px]">
               <p className="font-bold text-[30px]">SpiderG</p>
               <p className="text-[20px]">
@@ -136,8 +138,8 @@ export default function Home() {
       <div className="font-mono text-[#010326] text-center" id="Skills">
         <div className="pt-6 pb-10">
           <p className="text-[60px] font-bold mb-6">Skills</p>
-          <div className="flex justify-between max-w-[50%] mx-auto">
-            <div className="w-[300px]">
+          <div className="flex flex-col justify-between max-w-[50%] mx-auto md:flex-row">
+            <div className="w-[300px] mb-8 md:mb-0">
               <p className="font-bold text-[30px] mb-2">Technical Skills</p>
               <p className="text-[20px]">C++, C#, Next JS, HTML5, CSS3, Tailwindcss, JavaScript, Git, SQL, Illustrator, Photoshop</p>
             </div>
@@ -153,12 +155,12 @@ export default function Home() {
       <div className="text-center bg-[#393E59] text-[#F0F2F2] font-mono" id="ContactMe">
         <div className="py-6">
           <p className="text-[40px] mb-6 font-bold">Contact Me</p>
-          <ul className="flex justify-between max-w-[50%] mx-auto">
-            <li className="text-center">
+          <ul className="flex flex-col justify-between max-w-[50%] mx-auto md:flex-row">
+            <li className="mb-8 text-center md:mb-0">
               <p className="mb-2 font-bold text-[20px]">Call me on</p>
               <p className="text-[18px]">+91-9724241868</p>
             </li>
-            <li className="text-center">
+            <li className="mb-8 text-center md:mb-0">
               <p className="mb-2 font-bold text-[20px]">Email me on</p>
               <p className="text-[18px]">prajapatiharshil3107@gmail.com</p>
             </li>
